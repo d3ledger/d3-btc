2f8cf33568eb8cdf435f04dc60da6dfa0bf9694c
Anton Dolgopolov <dolgopolov@soramitsu.co.jp>
2019-06-20 15:04:53 +0300

    More withdrawal logs

---
+++ btc/src/main/kotlin/com/d3/btc/helper/output/TxOutputExt.kt
@@ -0,0 +1,10 @@
+package com.d3.btc.helper.output
+
+import com.d3.btc.helper.address.outPutToBase58Address
+import org.bitcoinj.core.TransactionOutput
+
+/**
+ * Returns brief UTXO information
+ */
+fun TransactionOutput.info() =
+    "Address ${outPutToBase58Address(this)} tx hash ${this.parentTransactionHash} value ${this.value}"
