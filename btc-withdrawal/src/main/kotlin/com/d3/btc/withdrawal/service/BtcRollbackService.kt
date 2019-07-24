66b04bb81a2c80979612623899e69168a8565288
Anton Dolgopolov <dolgopolov@soramitsu.co.jp>
2019-07-24 14:28:04 +0300

    Update rollback description

--- btc-withdrawal/src/main/kotlin/com/d3/btc/withdrawal/service/BtcRollbackService.kt
+++ btc-withdrawal/src/main/kotlin/com/d3/btc/withdrawal/service/BtcRollbackService.kt
@@ -8,12 +8,14 @@ package com.d3.btc.withdrawal.service
 import com.d3.btc.config.BTC_ASSET
 import com.d3.btc.helper.currency.satToBtc
 import com.d3.btc.withdrawal.transaction.WithdrawalDetails
+import com.d3.commons.sidechain.iroha.ROLLBACK_DESCRIPTION
 import com.d3.commons.sidechain.iroha.consumer.MultiSigIrohaConsumer
 import com.d3.commons.sidechain.iroha.util.ModelUtil
 import com.github.kittinunf.result.flatMap
 import mu.KLogging
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
+import kotlin.math.min
 
 /**
  * Bitcoin rollback service
@@ -31,13 +33,14 @@ class BtcRollbackService(
      */
     fun rollback(withdrawalDetails: WithdrawalDetails, reason: String) {
         val amountToRollbackSat = withdrawalDetails.amountSat + withdrawalDetails.withdrawalFeeSat
+        val rollbackMessage = "$ROLLBACK_DESCRIPTION. $reason"
         withdrawalConsumer.getConsumerQuorum().flatMap { quorum ->
             ModelUtil.transferAssetIroha(
                 withdrawalConsumer,
                 withdrawalConsumer.creator,
                 withdrawalDetails.sourceAccountId,
                 BTC_ASSET,
-                "Rollback. $reason",
+                rollbackMessage.substring(0, min(rollbackMessage.length, 64)).toLowerCase(),
                 satToBtc(amountToRollbackSat).toPlainString(),
                 withdrawalDetails.withdrawalTime,
                 quorum
