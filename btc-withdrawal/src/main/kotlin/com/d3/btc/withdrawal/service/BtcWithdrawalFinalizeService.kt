4c41d7c60fa7f860a3ee93daa5a2619d3bdf1c89
Anton Dolgopolov <dolgopolov@soramitsu.co.jp>
2019-07-26 11:23:40 +0300

    Fixed build

--- btc-withdrawal/src/main/kotlin/com/d3/btc/withdrawal/service/BtcWithdrawalFinalizeService.kt
+++ btc-withdrawal/src/main/kotlin/com/d3/btc/withdrawal/service/BtcWithdrawalFinalizeService.kt
@@ -8,7 +8,7 @@ package com.d3.btc.withdrawal.service
 import com.d3.btc.config.BTC_ASSET
 import com.d3.btc.helper.currency.satToBtc
 import com.d3.btc.withdrawal.transaction.WithdrawalDetails
-import com.d3.commons.service.FinalizationDetails
+import com.d3.commons.service.WithdrawalFinalizationDetails
 import com.d3.commons.service.WithdrawalFinalizer
 import com.github.kittinunf.result.Result
 import com.github.kittinunf.result.map
@@ -27,7 +27,7 @@ class BtcWithdrawalFinalizeService(private val withdrawalFinalizer: WithdrawalFi
      */
     fun finalize(withdrawalDetails: WithdrawalDetails): Result<Unit, Exception> {
         return withdrawalFinalizer.finalize(
-            FinalizationDetails(
+            WithdrawalFinalizationDetails(
                 satToBtc(withdrawalDetails.amountSat),
                 BTC_ASSET,
                 satToBtc(withdrawalDetails.withdrawalFeeSat),
