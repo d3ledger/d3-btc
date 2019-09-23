7dd3215036044b481954b01d5e5cd2fa8e0e18d7
Anton Dolgopolov <dolgopolov@soramitsu.co.jp>
2019-09-23 11:19:02 +0300

    CAS registration

--- btc-registration/src/main/kotlin/com/d3/btc/registration/strategy/BtcRegistrationStrategyImpl.kt
+++ btc-registration/src/main/kotlin/com/d3/btc/registration/strategy/BtcRegistrationStrategyImpl.kt
@@ -9,10 +9,16 @@ import com.d3.btc.provider.BtcFreeAddressesProvider
 import com.d3.btc.provider.BtcRegisteredAddressesProvider
 import com.d3.btc.provider.account.IrohaBtcAccountRegistrator
 import com.d3.commons.registration.RegistrationStrategy
+import com.d3.commons.sidechain.iroha.consumer.status.ToriiErrorResponseException
 import com.github.kittinunf.result.Result
 import com.github.kittinunf.result.flatMap
 import com.github.kittinunf.result.map
+import mu.KLogging
 import org.springframework.stereotype.Component
+
+private const val BAD_OLD_VALUE_ERROR_CODE = 4
+private const val COMPARE_AND_SET_DETAIL_COMMAND = "compareAndSetAccountDetail"
+private const val MAX_CAS_ATTEMPTS = 15
 
 //Strategy for registering BTC addresses
 @Component
@@ -29,7 +35,6 @@ class BtcRegistrationStrategyImpl(
      * @param publicKey - client public key
      * @return associated BTC address
      */
-    @Synchronized
     override fun register(
         accountName: String,
         domainId: String,
@@ -41,22 +46,73 @@ class BtcRegistrationStrategyImpl(
                     throw IllegalStateException("Not able to register $accountName@$domainId")
                 }
             }
-            .flatMap { btcFreeAddressesProvider.getFreeAddress() }
-            .flatMap { freeAddress ->
-                irohaBtcAccountCreator.create(
-                    freeAddress.address,
-                    accountName,
-                    domainId,
-                    freeAddress.info.notaryKeys,
-                    freeAddress.info.nodeId
-                ) {
-                    btcFreeAddressesProvider.addRegisterFreeAddressCommands(it, freeAddress)
-                }
+            .flatMap {
+                registerCAS(accountName, domainId)
             }
     }
+
+    /**
+     * Registers account using Iroha CAS
+     * @param accountName - account name
+     * @param domainId - domain id
+     * @return registered address
+     */
+    private fun registerCAS(accountName: String, domainId: String): Result<String, Exception> {
+        var attempts = 1
+        var stopRegistration = false
+        var result: Result<String, Exception>? = null
+        while (!stopRegistration) {
+            if (attempts >= MAX_CAS_ATTEMPTS) {
+                return Result.error(Exception("Cannot register $accountName@$domainId. Too many CAS attempts."))
+            }
+            // Get free address
+            btcFreeAddressesProvider.getFreeAddress()
+                .flatMap { freeAddress ->
+                    // Try to register
+                    irohaBtcAccountCreator.create(
+                        freeAddress.address,
+                        accountName,
+                        domainId,
+                        freeAddress.info.notaryKeys,
+                        freeAddress.info.nodeId
+                    ) {
+                        btcFreeAddressesProvider.addRegisterFreeAddressCommands(it, freeAddress)
+                    }
+                }.fold({
+                    logger.info("User $accountName@$domainId has been registered. CAS attempts $attempts")
+                    // Stop the loop and return result if everything was ok
+                    stopRegistration = true
+                    result = Result.of(it)
+                }, { ex ->
+                    if (isCASError(ex)) {
+                        // Wait a little before next attempt
+                        attempts++
+                        // Go on next iteration and try again if an error occurred due to CAS issues
+                        logger.warn("Cannot register $accountName@$domainId due to CAS issues. Try again. CAS attempts $attempts")
+                    } else {
+                        // Stop the loop and return an exception
+                        stopRegistration = true
+                        result = Result.error(ex)
+                    }
+                })
+        }
+        return result!!
+    }
+
+    /**
+     * Checks if a given exception is related to CAS issues
+     * @param ex - exception to check
+     * @return true if it's related to CAS issues
+     */
+    private fun isCASError(ex: Exception) =
+        ex is ToriiErrorResponseException &&
+                ex.toriiResponse.errorCode == BAD_OLD_VALUE_ERROR_CODE &&
+                ex.toriiResponse.errOrCmdName == COMPARE_AND_SET_DETAIL_COMMAND
 
     /**
      * Get number of free addresses.
      */
     override fun getFreeAddressNumber() = btcFreeAddressesProvider.countFreeAddresses()
+
+    companion object : KLogging()
 }
